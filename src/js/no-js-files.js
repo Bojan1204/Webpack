ebc695ee3a739bfd06e713a3f6f477b541b0c560
Bojan <bojan.bosnic97@gmail.com>
2021-02-16 00:52:42 +0100

    first commit

--- src/js/no-js-files.js
+++ src/js/no-js-files.js
@@ -3,7 +3,7 @@
 // import '../img/webpack4.jpg';
 // import '../img/bg.png';
 import '../scss/index.scss';
-// import '../img/icons/favicon.ico';
+import '../img/icons/favicon.ico';
 import '../img/icons/favicon.png';
 import '../../.gitkeep';
 import '../.htaccess';
